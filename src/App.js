a60408f0f330866a9a518ec3fe40bd2dd81fb168
Deepika Surisetti <deepika.surisetti@zapcg.com>
2025-01-17 15:18:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import Calci from './Calci';
+
+function App() {
+  return (
+    <div className="App">
+       <Calci></Calci>
+    </div>
+  );
+}
+
+export default App;
